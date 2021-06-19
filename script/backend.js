b1f6ab3eb7b452673f2f9a34fd7ac8350a61cb1b
CharlieYu4994 <charlieyu4994@outlook.com>
2021-06-19 23:55:50 +0800

    不懂，不作为

--- script/backend.js
+++ script/backend.js
@@ -1,14 +1,12 @@
-function setdomain(id) {
+function getdomain() {
     var url = "/config.json"
     var request = new XMLHttpRequest();
     request.open("get", url);
     request.send(null);
     request.onload = function () {
         if (request.status == 200) {
-            console.log(request.responseText)
             var backend = JSON.parse(request.responseText)["backend"];
-            var form = document.getElementId(id);
-            form.action = backend
+            return backend
         }
     }
 }
