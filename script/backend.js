8356989db981235c2ec359346b5efa1358938fe8
CharlieYu4994 <charlieyu4994@outlook.com>
2021-07-05 14:17:47 +0800

    query

--- script/backend.js
+++ script/backend.js
@@ -3,7 +3,26 @@ function getdomain() {
     var request = new XMLHttpRequest();
     request.open("get", url, false);
     request.send(null);
-    if (request.status == 200) {
+    if (request.readyState == 4) {
         return JSON.parse(request.responseText)["backend"];
+    }
+}
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var res = window.location.search.substr(1).match(reg);
+    if (res != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+ }
+
+function gettext(url) {
+    var request = new XMLHttpRequest();
+    var arg = getQueryString("k")
+    request.open("get", url+"/get?k="+arg, false)
+    request.send(null)
+    if (request.readyState == 4) {
+        return request.responseText()
     }
 }
