e6de2ab43561decae3dd039767474afaba1c081f
CharlieYu4994 <charlieyu4994@outlook.com>
2021-07-22 15:57:29 +0800

    删除跳转

--- script/backend.js
+++ script/backend.js
@@ -21,9 +21,9 @@ function getQueryString(name) {
     return null;
 }
 
-function gettext(url, arg) {
+function gettext(url, key) {
     var request = new XMLHttpRequest();
-    request.open("get", url + "/get?k=" + arg, false);
+    request.open("get", url + "/get?k=" + key, false);
     request.send(null);
     if (request.readyState == 4) {
         if (request.status == 200) {
@@ -39,13 +39,36 @@ function sendFormData(url, form) {
     var fd = new FormData(form);
 
     request.open("POST", url+"/add");
-    request.withCredentials=true;
     request.send(fd);
-    request.addEventListener("load", function () {
+    request.addEventListener("load", function() {
         if (request.status == 200) {
             window.location.href = "/get?k=" + request.responseText
         } else {
-            alert("提交失败")
+            alert("提交失败");
+        }
+    });
+}
+
+function del(url, key) {
+    var request = new XMLHttpRequest();
+    request.withCredentials = true;
+    request.open("GET", url + "/del?k=" + key);
+    request.send(null);
+    request.addEventListener("load", function() {
+        switch (request.status) {
+            case 403:
+                alert("这不是你上传的 Paste");
+                break;
+            case 500:
+                alert("Paste 不存在");
+                break;
+            case 200:
+                alert("删除成功");
+                window.location.href = "/";
+                break;
+            default:
+                alert("未知错误");
+                break;
         }
     });
 }
